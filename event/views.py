de3ac42bf3662d7833f83e2f2e9e38a84d8c61d7
lOlIl <peter.margetiak@gmail.com>
2012-07-05 18:33:22 +0200

    * updated URL, simple views for event app

--- event/views.py
+++ event/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse("HOME page")
+
+def index(request):
+	return HttpResponse("Hello Event list")
+
+def detail(request, event_id):
+	return HttpResponse("Hello Event detail %s" % event_id)
