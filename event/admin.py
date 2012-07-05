3915c523d4b6f70dbdd10bec39193fc6ff0c5ca5
lOlIl <peter.margetiak@gmail.com>
2012-07-05 22:21:53 +0200

    * updated admin page

--- event/admin.py
+++ event/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from event.models import Event, Team, Member
+from event.models import *
 
 class EventAdmin(admin.ModelAdmin):
-	fields = ['name', 'time_created', 'time_started', 'time_end']
+	fields = ['name', 'time_started', 'time_end']
 
 admin.site.register(Event, EventAdmin)
-admin.site.register(Team)
 admin.site.register(Member)
+admin.site.register(Team)
+admin.site.register(Teammate)
+
